67d23cdfeea04d65fb5d2098420161569b57a3a7
inclee <inclee@163.com>
2019-08-16 08:16:52 +0800

    change struct

--- activity/activity.go
+++ activity/activity.go
@@ -8,27 +8,26 @@ import (
 
 type Verb int
 
-
-const(
-	ActObject_Type_Body = 1
+const (
+	ActObject_Type_Body  = 1
 	ActObject_Type_Thing = 2
 )
 
-type  ActObject struct{
-	Id int `json:id`
+type ActObject struct {
+	Id   int `json:id`
 	Type int `json:type`
 }
 
 type BaseActivty struct {
-	Actor  int `json:"actor"`
+	Actor  int       `json:"actor"`
 	Verb   Verb      `json:"verb"`
 	Object ActObject `json:"object"`
-	Target int `json:"target"`
+	Target int       `json:"target"`
 	Time   time.Time `json:"time"`
 	Extra  string    `json:"extra"`
 }
 
-func NewActivity(actor int, verb Verb, object int, target int, time time.Time, extra string) *BaseActivty {
+func NewActivity(actor int, verb Verb, object ActObject, target int, time time.Time, extra string) *BaseActivty {
 	act := new(BaseActivty)
 	act.Actor = actor
 	act.Verb = verb
