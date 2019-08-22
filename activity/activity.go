4bcbd4743fac324d376c915ee61724bd0c745382
lixu <lixu@optaim.com>
2019-08-22 19:36:21 +0800

    add private

--- activity/activity.go
+++ activity/activity.go
@@ -25,13 +25,16 @@ type BaseActivty struct {
 	Extra  string    `json:"extra"`
 }
 
-func NewActivity(actor int, verb Verb, object ActObject, target int, time time.Time, extra string) *BaseActivty {
+func NewActivity(actor int, verb Verb, object ActObject, target int, time time.Time,private bool,allow []int,deny []int, extra string) *BaseActivty {
 	act := new(BaseActivty)
 	act.Actor = actor
 	act.Verb = verb
 	act.Object = object
 	act.Target = target
 	act.Time = time
+	act.Private = private
+	act.Allow = allow
+	act.Deny = deny
 	act.Extra = extra
 	return act
 }
